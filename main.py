d73e36be0be2dd710272f6f0fb6feb932aadba47
Date44 <mgura1833@gmail.com>
2025-10-01 11:28:01 +0200

    fix

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ dp = Dispatcher()
 list1 = [0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1, 37, 27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2]
 
 def get_data_file(user_id):
-    return f'numbers_data_{user_id}.json'
+    return f'numbers_data_843192448.json'
 
 def load_data(user_id):
     data_file = get_data_file(user_id)
@@ -112,18 +112,16 @@ async def number_handler(message: Message):
                 next_numbers.append(lst[i + 1])
     if next_numbers:
         counter = Counter(next_numbers)
+        total = sum(counter.values())
         top3 = counter.most_common(3)
         text = 'TOP 3 numbers that go after this number:\n'
         for n, count in top3:
+            percent = round(count / total * 100, 2)
             if n in list1:
                 idx = list1.index(n)
-                before = list1[idx - 1] if idx > 0 else list1[-1]
-                after = list1[idx + 1] if idx < len(list1) - 1 else list1[0]
-                text += f'{before if before != 37 else "00"} '
-                text += f'{n if n != 37 else "00"} '
-                text += f'{after if after != 37 else "00"} ({count} раз)\n'
+                text += f'{n if n != 37 else "00"} ({percent}%)\n'
             else:
-                text += f'{n if n != 37 else "00"} (not in the main list) ({count} once)\n'
+                text += f'{n if n != 37 else "00"} (not in the main list) ({percent}%)\n'
     else:
         text = 'There is no data on the following numbers after that.'
     await message.answer(text)

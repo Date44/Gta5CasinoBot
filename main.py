36e7c63e5ed0c6465034727bdc7d6254ab51980e
Date44 <mgura1833@gmail.com>
2025-10-01 12:11:22 +0200

    fix

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ dp = Dispatcher()
 list1 = [0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1, 37, 27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2]
 
 def get_data_file(user_id):
-    return f'numbers_data_843192448.json'
+    return f'numbers_data_{user_id}.json'
 
 def load_data(user_id):
     data_file = get_data_file(user_id)
